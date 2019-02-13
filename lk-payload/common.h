1dcf3bbfac29d12d9007ada456f960af85fd073b
Joel Stein <js@funkturm.net>
2019-02-13 07:45:50 +0100

    working exploit: lk-payload

--- lk-payload/common.h
+++ lk-payload/common.h
@@ -6,13 +6,14 @@ struct device_t {
     uint32_t unk3;
     uint32_t unk4;
     size_t (*read)(struct device_t *dev, uint64_t dev_addr, void *dst, uint32_t size, uint32_t part);
+    size_t (*write)(struct device_t *dev, void *src, uint64_t block_off, size_t sz, uint32_t part);
 };
 
-//struct device_t* (*get_device)() = (void*)0x81E14681;
 struct device_t* (*get_device)() = (void*)0x81E354F1;
-//void (*cache_clean)(void *addr, size_t sz) = (void*)0x81E1AE60;
 void (*cache_clean)(void *addr, size_t sz) = (void*)0x81E3BCD0;
-//void (*cache_clean)(void *addr, size_t sz) = (void*)0x81E1AE34;
+size_t (*video_printf)(const char *format, ...) = (void *)0x81E42289;
+
+uint32_t* g_boot_mode = (uint32_t*) 0x81E6D370;
 
 #define PAYLOAD_DST 0x8AB00000
 #define PAYLOAD_SRC 0x200000
--- lk-payload/main.c
+++ lk-payload/main.c
@@ -4,7 +4,7 @@
 
 #include "common.h"
 
-#define RELOAD_LK 1
+//#define RELOAD_LK
 
 void low_uart_put(int ch) {
     volatile uint32_t *uart_reg0 = (volatile uint32_t*)0x11002014;
@@ -23,50 +23,16 @@ void _putchar(char character)
     low_uart_put(character);
 }
 
-//int (*original_read)(struct device_t *dev, uint64_t block_off, void *dst, size_t sz, int part) = (void *)0x4BD1E839;
-int (*original_read)(struct device_t *dev, uint64_t block_off, void *dst, size_t sz, int part) = (void *)0x81E141F5;
+int (*original_read)(struct device_t *dev, uint64_t block_off, void *dst, size_t sz, uint32_t part) = (void *)0x81E35065;
+int (*original_write)(struct device_t *dev, void *src, uint64_t block_off, size_t sz, uint32_t part) = (void *)0x81E351E5;
 
-uint64_t g_boot, g_recovery, g_lk;
-
-void hex_dump(const void* data, size_t size) {
-    char ascii[17];
-    size_t i, j;
-    ascii[16] = '\0';
-    for (i = 0; i < size; ++i) {
-        printf("%02X ", ((unsigned char*)data)[i]);
-        if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
-            ascii[i % 16] = ((unsigned char*)data)[i];
-        } else {
-            ascii[i % 16] = '.';
-        }
-        if ((i+1) % 8 == 0 || i+1 == size) {
-            printf(" ");
-            if ((i+1) % 16 == 0) {
-                printf("\n");
-                // printf("|  %s \n", ascii);
-            } else if (i+1 == size) {
-                ascii[(i+1) % 16] = '\0';
-                if ((i+1) % 16 <= 8) {
-                    printf(" ");
-                }
-                for (j = (i+1) % 16; j < 16; ++j) {
-                    printf("   ");
-                }
-                // printf("|  %s \n", ascii);
-                printf("\n");
-            }
-        }
-    }
-}
-
+uint64_t g_boot, g_recovery, g_lk, g_misc;
 
 int read_func(struct device_t *dev, uint64_t block_off, void *dst, size_t sz, int part) {
     printf("read_func hook\n");
     int ret = 0;
-    //hex_dump((void *)0x81E6E998, 0x1000);
-    //return original_read(dev, block_off, dst, sz, part);
     if (block_off == g_boot * 0x200 || block_off == g_recovery * 0x200) {
-        //hex_dump((void *)0x81E6E998, 0x1000);
+        //hex_dump((void *)0x81E6C000, 0x100);
         printf("demangle boot image - from 0x%08X\n", __builtin_return_address(0));
         if (sz < 0x400) {
             ret = original_read(dev, block_off + 0x400, dst, sz, part);
@@ -80,17 +46,15 @@ int read_func(struct device_t *dev, uint64_t block_off, void *dst, size_t sz, in
         printf("read_func original_read\n");
         ret = original_read(dev, block_off, dst, sz, part);
     }
-    if(__builtin_return_address(0) == (void*)0x81E02D13){
-      //void (*my_ret)() = (void*)0x81E02D63;
-      //void (*my_ret)() = (void*)0x81E02DA7;
-      //my_ret();
-
-//      asm volatile (
-//          "b 0x81E02D36\n"
-//          : );
-    }
     return ret;
 }
+
+#if 0
+int write_func(struct device_t *dev, void *src, uint64_t block_off, size_t sz, int part) {
+    printf("write_func hook\n");
+    return original_write(dev, src, block_off, sz, part);
+}
+#endif
 
 static void parse_gpt() {
     uint8_t raw[0x800] = { 0 };
@@ -110,6 +74,9 @@ static void parse_gpt() {
         } else if (memcmp(name, "U\x00\x42\x00O\x00O\x00T\x00\x00\x00", 12) == 0) {
             printf("found lk at 0x%08X\n", start);
             g_lk = start;
+        } else if (memcmp(name, "M\x00I\x00S\x00\x43\x00\x00\x00", 10) == 0) {
+            printf("found misc at 0x%08X\n", start);
+            g_misc = start;
         }
     }
 }
@@ -117,166 +84,144 @@ static void parse_gpt() {
 int main() {
     int ret = 0;
     printf("This is LK-payload by xyz. Copyright 2019\n");
-
-    //uint32_t **argptr = (void*)0x4BD00020;
-    uint32_t **argptr = (void*)0x81E00020;
-    uint32_t *arg = *argptr;
-    //arg[0x53] = 4; // force 64-bit linux kernel
+    printf("Ported to Fire 7 by k4y0z, Copyright 2019\n");
 
     int fastboot = 0;
-
-    /*
-    [300] [LK/LCM] lcm_init enter, build type: PVT, vendor type: FITI_KD
-    [300] [LK/LCM] lcm_init No LCM connected. Just Return
-    [340] DSI_WaitForNotBusy:Error:DSI_INTSTA is 0...
-    */
 
     parse_gpt();
 
     if (!g_boot || !g_recovery || !g_lk) {
         printf("failed to find boot, recovery or lk\n");
-        while (1) {}
+        printf("falling back to fastboot mode\n");
+        fastboot = 1;
     }
 
-#if 0
-    //int (*app)() = (void*)0x4BD27109;
-    int (*app)() = (void*)0x81E3D8E5; //loading
-    //int (*app)() = (void*)0x81E3D4AD;
-    //int (*app)() = (void*)0x81E1C2F0;
-    //int (*app)() = (void*)0x81E0074C;
-    //int (*app)() = (void*)0x81E39B7D;
-    //int (*app2)() = (void*)0x81E1C2F1; //loading
+    int (*app)() = (void*)0x81E3DD25;
 
-    /*unsigned char overwritten[80] = {
-        0xE9, 0x0A, 0xD0, 0x4B, 0x7D, 0x0E, 0xD0, 0x4B, 0x01, 0x09, 0xD0, 0x4B, 0x31, 0x0B, 0xD0, 0x4B,
-        0x9D, 0x0C, 0xD0, 0x4B, 0x00, 0x84, 0xD5, 0x4B, 0x05, 0x0A, 0xD0, 0x4B, 0x71, 0x0A, 0xD0, 0x4B,
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x2D, 0x1B, 0xD0, 0x4B,
-        0xF9, 0x1C, 0xD0, 0x4B, 0xA9, 0x1A, 0xD0, 0x4B, 0x95, 0x1D, 0xD0, 0x4B, 0x19, 0x1A, 0xD0, 0x4B,
-        0xED, 0x1B, 0xD0, 0x4B, 0xA5, 0x19, 0xD0, 0x4B, 0x81, 0x1C, 0xD0, 0x4B, 0x00, 0x00, 0x00, 0x00 
-    };*/
     unsigned char overwritten[] = {
-        0xB1, 0xD5, 0xE1, 0x81, 0x6D, 0xD7, 0xE1, 0x81, 0xA1, 0xD3, 0xE1, 0x81, 0xFD, 0xD5, 0xE1, 0x81,
-        0x69, 0xD7, 0xE1, 0x81, 0x98, 0xC1, 0xE6, 0x81, 0xBD, 0xD4, 0xE1, 0x81, 0x31, 0xD5, 0xE1, 0x81,
-        0xD5, 0xD0, 0xE1, 0x81, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x71, 0x12, 0xE0, 0x81, 0x39, 0x14, 0xE0, 0x81, 0x49, 0x10, 0xE0, 0x81, 0xC1, 0x12, 0xE0, 0x81,
+        0x35, 0x14, 0xE0, 0x81, 0x00, 0x84, 0xE6, 0x81, 0x65, 0x11, 0xE0, 0x81, 0xE5, 0x11, 0xE0, 0x81,
     };
 
-    memcpy((void*)0x81E6E998, overwritten, sizeof(overwritten));
-
-    //void *lk_dst = (void*)0x4BD00000;
-    void *lk_tmp = (void*)0x89B00000;
     void *lk_dst = (void*)0x81E00000;
     #define LK_SIZE (0x800 * 0x200)
 
     struct device_t *dev = get_device();
 
-    if(RELOAD_LK) {
-      ret = dev->read(dev, g_lk * 0x200 + 0x200, lk_tmp, LK_SIZE, USER_PART);
-      printf("read lk: 0x%08X\n", ret);
-    }
+    memcpy((void*)0x81E6C000, overwritten, sizeof(overwritten));
 
-    uint8_t tmp[0x10] = { 0 };
-    dev->read(dev, g_boot * 0x200 + 0x400, tmp, 0x10, USER_PART);
-    if (strcmp(tmp, "FASTBOOT_PLEASE") == 0) {
-        printf("well since you're asking so nicely...\n");
+    uint8_t bootloader_msg[0x10] = { 0 };
+
+    // Read amonet-flag from MISC partition
+    //dev->read(dev, g_misc * 0x200 + 0x4000, bootloader_msg, 0x10, USER_PART);
+    dev->read(dev, g_misc * 0x200, bootloader_msg, 0x10, USER_PART);
+    //video_printf("%s\n", bootloader_msg);
+
+
+    //uint8_t tmp[0x10] = { 0 };
+    //dev->read(dev, g_boot * 0x200 + 0x400, tmp, 0x10, USER_PART);
+    uint8_t *tmp = (void*)0x81E6C3B0;
+
+    // microloader
+    if (strncmp(tmp, "FASTBOOT_PLEASE", 15) == 0 ) {
+      fastboot = 1;
+    }
+    // flag on MISC
+    else if(strncmp(bootloader_msg, "boot-amonet", 11) == 0) {
+      fastboot = 1;
+      // reset flag
+      memset(bootloader_msg, 0, 11);
+      dev->write(dev, bootloader_msg, g_misc * 0x200, 11, USER_PART);
+    }
+    // factory and factory advanced boot
+    else if(*g_boot_mode == 4 || *g_boot_mode == 6){
         fastboot = 1;
     }
 
-    if (RELOAD_LK){
+#ifdef RELOAD_LK
       printf("Disable interrupts\n");
       asm volatile ("cpsid if");
-
-      printf("Copy lk\n");
-      cache_clean(lk_tmp, LK_SIZE);
-      memcpy(lk_dst, lk_tmp, LK_SIZE);
-
-      uint32_t *argfix = (void*)0x81E00020;
-      *argfix = 0x82053C08;
-    }
+#endif
 
     uint16_t *patch;
 
-
     // force fastboot mode
     if (fastboot) {
-        patch = (void*)0x81E3D914;
+        printf("well since you're asking so nicely...\n");
+
+        patch = (void*)0x81E3DD54;
         *patch = 0xE003;
+
+        video_printf("=> HACKED FASTBOOT mode: (%d) - xyz, k4y0z\n", *g_boot_mode);
+    }
+    else if(*g_boot_mode == 2) {
+        video_printf("=> RECOVERY mode...");
     }
     
-    /*
-    // enable all commands
-    patch = (void*)0x4BD0D838;
-    *patch++ = 0x2000; // movs r0, #0
-    *patch = 0x4770;   // bx lr
-    */
-
-    /*
     // device is unlocked
-    patch = (void*)0x81E3CE7C;
+    patch = (void*)0x81E20B40;
     *patch++ = 0x2001; // movs r0, #1
     *patch = 0x4770;   // bx lr
-    
+
+    // This enables adb-root-shell
+    // amzn_verify_limited_unlock (to set androidboot.unlocked_kernel=true)
+    patch = (void*)0x81E20D60;
+    *patch++ = 0x2000; // movs r0, #0
+    *patch = 0x4770;   // bx lr
+
+#if 0
     // skip verification
-    patch = (void*)0x81E39962;
-    while(patch < (uint16_t*)0x81E39980){
+    patch = (void*)0x81E1D39A;
+    while(patch < (uint16_t*)0x81E1D3C8){
       *patch++ = 0xBF00;
     }
+#endif
 
-    // engineering device
-    patch = (void*)0x81E00240;
-    *patch = 0x2000;
-    */
+    //ignore failed authentication (but still verify)
+    patch = (void*)0x81E1D419;
+    *patch = 0x2001; // movs r0, #0
+
+    // Force uart enable
+    char* disable_uart = (char*)0x81E58645;
+    strcpy(disable_uart, "printk.disable_uart=0");
 
     uint32_t *patch32;
-    patch32 = (void*)0x81E02D2E;
-    *patch32 = 0xE3B00000; // mov r0, #0
-    patch32 = (void*)0x81E02DBC;
-    *patch32 = 0xE3B00000; // mov r0, #0
 
+    if(!fastboot) {
+      // hook bootimg read function
+      original_read = (void*)dev->read;
+      original_write = (void*)dev->write;
 
-    // amzn_verify_unlock
-    //patch = (void*)0x81E3CCD8;
-    //*patch++ = 0x2001; // movs r0, #1
-    //*patch = 0x4770;   // bx lr
+      patch32 = (void*)0x81E6478C;
+      *patch32 = (uint32_t)read_func;
 
-    // hook bootimg read function
-    original_read = (void*)dev->read;
-    //patch32 = (void*)0x81E67124;
-    if(RELOAD_LK) {
-      patch32 = (void*)0x81E63124;
-    }
-    else {
       patch32 = (void*)&dev->read;
+      *patch32 = (uint32_t)read_func;
     }
-    *patch32 = (uint32_t)read_func;
 
-    /*
-    patch32 = (void*)0x4BD681B8;
-    *patch32 = 1; // // force 64-bit linux kernel
-    */
-    
+#if 0
+    // hook write-function
+    patch32 = (void*)&dev->write;
+    *patch32 = (uint32_t)write_func;
+#endif
+
     printf("Clean lk\n");
     cache_clean(lk_dst, LK_SIZE);
 
-    if(RELOAD_LK) {
-      printf("About to jump to LK\n");
+#ifdef RELOAD_LK
+    printf("About to jump to LK\n");
 
-      asm volatile (
-          "mov r4, %0\n" 
-          "mov r3, %1\n"
-          "blx r3\n"
-          : : "r" (arg), "r" (lk_dst) : "r3", "r4");
+    asm volatile (
+        "mov r4, %0\n" 
+        "mov r3, %1\n"
+        "blx r3\n"
+        : : "r" (arg), "r" (lk_dst) : "r3", "r4");
 
-      printf("Failure\n");
-    }
-    else {
-      app();
-    }
-
+    printf("Failure\n");
+#else
+    app();
 #endif
+
     while (1) {
 
     }
