2933dd63e7f7ed2b24cf40113f60624f228b0cb6
Joel Stein <js@funkturm.net>
2019-07-21 04:00:20 +0200

    Add write to device-struct

--- lk-payload/common.h
+++ lk-payload/common.h
@@ -6,6 +6,7 @@ struct device_t {
     uint32_t unk3;
     uint32_t unk4;
     size_t (*read)(struct device_t *dev, uint64_t dev_addr, void *dst, uint32_t size, uint32_t part);
+    size_t (*write)(struct device_t *dev, void *src, uint64_t block_off, size_t size, uint32_t part);
 };
 
 struct device_t* (*get_device)() = (void*)0x4BD1EC99;
